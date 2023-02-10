d3fa3818d1778f5acf6c1127cbd48f8928a7aec0
Hhb <33392861+764777472@users.noreply.github.com>
2023-02-10 14:58:29 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,6 +14,9 @@ const openai = new OpenAIApi(configuration);
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use((req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+})
 
 app.get('/', async (req, res) => {
   res.status(200).send({
